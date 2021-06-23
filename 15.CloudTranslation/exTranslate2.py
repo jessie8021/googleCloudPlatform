597e04b15748028dab39beab45d9dd4f26299f63
Hyemi <jessie8021@gmail.com>
2021-06-23 12:40:19 +0900

    Translate sample code update

--- 15.CloudTranslation/exTranslate2.py
+++ 15.CloudTranslation/exTranslate2.py
@@ -6,10 +6,10 @@ parent = "projects/mysecont/locations/global"
 response = client.translate_text(
     request={
         "parent": parent,
-        "contents": "사과",
+        "contents": "Cloud Translation API에 번역을 요청하면 기본적으로 인공신경망 기계 번역(NMT) 모델을 사용하여 덱스트가 번역됩니다.",
         "mime_type": "text/plain",  # mime types: text/plain, text/html
         "source_language_code": "ko",
-        "target_language_code": "ja",
+        "target_language_code": "en",
     }
 )
 for translation in response.translations:
