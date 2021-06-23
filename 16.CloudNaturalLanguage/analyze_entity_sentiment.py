e908220d61289aa8cac8c019cbe027442485aa18
Hyemi <jessie8021@gmail.com>
2021-06-23 13:24:00 +0900

    cloud natural language sample code

---
+++ 16.CloudNaturalLanguage/analyze_entity_sentiment.py
@@ -0,0 +1,24 @@
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+
+client = language_v1.LanguageServiceClient()
+content = 'When I was young, there was an samazing publication called The Wole Earth Catalog, Which ' \
+          'was one of the bibles og my generation. It was created by a fellow named Stewart Brand not far ' \
+          'from here in Menlo Park, and he brought it to life with his poetic touch. This was in the late ' \
+          '1960s, before personal computers and desktop publishing, so it was all made with typewriters, scissors ' \
+          'and Polaroid cameras. It was sort of like Google in paperback form, 35 years before Google cate along: ' \
+          'It was idealistic, and overflowing with neat tools and great notions.'
+type_ = enums.Document.Type.PLAIN_TEXT
+gcs_content_url = 'gs://dataflow-sample-test/UptownFunk.txt'
+language = 'en'
+
+# content  항목감정분석
+document = {'content' : content, 'type' : type_, 'language' : language}
+
+# gcs 항목감정분석
+#document = {'gcs_content_uri' : gcs_content_url, 'type' : type_, 'language' : language}
+encoding_type = enums.EncodingType.UTF8
+
+response = client.analyze_entity_sentiment(document)
+
+print(response)
